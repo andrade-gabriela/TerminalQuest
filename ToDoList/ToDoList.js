c6bce03024379d563c1dcae9be536ce3905d476e
Gabriela <gabedeandradee@gmail.com>
2023-02-16 18:40:35 -0300

    Incrementando itens na lista com prompt

--- ToDoList/ToDoList.js
+++ ToDoList/ToDoList.js
@@ -1,13 +1,14 @@
 import inquirer from "inquirer";
 import fs from "fs";
 import chalk from "chalk";
+import { AddItem } from "./utils/Add.js";
 
 const questions = [
   {
     type: "list",
     name: "question1",
     message: "O que você quer fazer?",
-    choices: ["Adicionar", "Remover"],
+    choices: ["Adicionar", "Listar"],
   },
 ];
 
@@ -17,6 +18,25 @@ let Data = JSON.parse(rawData);
 inquirer.prompt(questions).then((answer) => {
   if (answer.question1 == "Adicionar") {
     console.log("Você vai adicionar");
-    console.log(Data);
+    getTask();
+  } else if (answer.question1 == "Listar") {
+    // console.log("Esta função ainda está sendo implementada..");
+    Data["Tasks"].forEach((element) => {
+      console.log("* " + element);
+    });
   }
 });
+
+function getTask() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "task",
+        message: "Insira sua tarefa",
+      },
+    ])
+    .then((answer) => {
+      AddItem(answer.task);
+    });
+}
--- ToDoList/utils/Add.js
+++ ToDoList/utils/Add.js
@@ -1,8 +1,15 @@
-export function AddItem(item) {
-  fs.writeFileSync(
-    "List.json",
-    JSON.stringify({
-      vc: "maniaco",
-    })
-  );
+import fs from "fs";
+import { v4 as uuid } from "uuid";
+
+export function AddItem(task, desc) {
+  let rawData = fs.readFileSync("List.json");
+  let Data = JSON.parse(rawData);
+
+  Data["Tasks"].push(task);
+
+  //JSON.stringify({
+  //   Task: item,
+  //   id: uuid(),
+  // })
+  fs.writeFileSync("List.json", JSON.stringify(Data));
 }
